e105eea977ead177820511787343d36832baf4d8
Kelvin Rojas <rc19023@ues.edu.sv>
2022-11-02 15:13:40 -0600

    feat: adding library querybuilder 	- adding feature to create a custom query and manage the payload conditionality 	- [Trello-Card] https://trello.com/c/wGMYH0Ie

---
+++ src/components/BuilderApp/BuilderApp.js
@@ -0,0 +1,40 @@
+import React, {useCallback, useState} from 'react';
+import { Query, Builder, Utils as QbUtils } from 'react-awesome-query-builder';
+import { config } from './config';
+import 'react-awesome-query-builder/lib/css/styles.css';
+import 'react-awesome-query-builder/lib/css/compact_styles.css';
+
+const queryValue = {"id": QbUtils.uuid(), "type": "group"};
+
+const BuilderApp = () => {
+    const [state, setState] = useState({
+        tree: QbUtils.checkTree(QbUtils.loadTree(queryValue), config),
+        config: config,
+    });
+
+    const onChange = useCallback((immutableTree, config) => {
+        setState({tree: immutableTree, config: config});
+    }, []);
+
+    const renderBuilder = useCallback((props) => (
+        <div className="query-builder-container" style={{padding: '10px'}}>
+            <div className="query-builder qb-lite">
+                <Builder {...props} />
+            </div>
+        </div>
+    ), []);
+
+    return (
+        <div className="App">
+            <Query
+                {...config}
+                value={state.tree}
+                onChange={onChange}
+                renderBuilder={renderBuilder}
+            />
+        </div>
+    );
+
+}
+
+export default BuilderApp;
---
+++ src/components/BuilderApp/config.js
@@ -0,0 +1,40 @@
+import BootstrapConfig from 'react-awesome-query-builder/lib/config/bootstrap';
+
+const initialConfig = BootstrapConfig;
+
+export const config = {
+    ...initialConfig,
+    fields: {
+        qty:{
+            label: 'Quantity',
+            type: 'number',
+            valueSources: ['value'],
+            preferWidgets: ['number'],
+            fieldSettings: {
+                min: 0,
+            },
+        },
+        price:{
+            label: 'Price',
+            type: 'number',
+            valueSources: ['value'],
+            preferWidgets: ['slider', 'rangeslider'],
+            fieldSettings: {
+                min: 0,
+                max: 100,
+            },
+        },
+        color: {
+            label: 'Color',
+            type: 'select',
+            valueSources: ['value'],
+            fieldSettings: {
+                listValues: [
+                    { value: 'yellow', title: 'Yellow' },
+                    { value: 'green', title: 'Green' },
+                    { value: 'orange', title: 'Orange' },
+                ],
+            },
+        }
+    }
+}
