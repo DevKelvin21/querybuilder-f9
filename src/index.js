21bf6c9181ba7d09caa36058db08626d31374219
Kelvin Rojas <rc19023@ues.edu.sv>
2022-11-07 18:24:25 -0600

    style: adding bootstrap to the project

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
